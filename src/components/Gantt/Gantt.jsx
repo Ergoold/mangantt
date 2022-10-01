ba36aa56556096a928c48efe04d1b981ce51005f
Ergoold <eran1gold@gmail.com>
2022-10-01 05:10:35 +0300

    Create Input element

--- src/components/Gantt/Gantt.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import './gantt.css';
-
-function Gantt() {
-  return <div className="gantt"></div>;
-}
-
-export default Gantt;
---
+++ src/components/Input/Input.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import './input.css';
+
+function Input() {
+  return <input className="input" />;
+}
+
+export default Input;
