39006218d13bbcd0f3177a4d5016d3de68f6d144
Ergoold <eran1gold@gmail.com>
2022-10-01 04:44:29 +0300

    Create Gantt component

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,13 @@
 import React from 'react';
+import Gantt from './components/Gantt/Gantt';
 import './app.css';
 
 function App() {
-  return <div className="app"></div>;
+  return (
+    <div className="app">
+      <Gantt></Gantt>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/Gantt/Gantt.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import './gantt.css';
+
+function Gantt() {
+  return <div className="gantt"></div>;
+}
+
+export default Gantt;
