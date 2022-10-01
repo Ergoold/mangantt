3b5542fad1de8b2244c3868df81fa8309b08225b
Ergoold <eran1gold@gmail.com>
2022-10-01 15:12:00 +0300

    Add components to Gantt

--- src/components/Gantt/Gantt.tsx
+++ src/components/Gantt/Gantt.tsx
@@ -1,13 +1,20 @@
-import React from 'react';
+import React, { memo } from 'react';
+import Chart from '../Chart/Chart';
 import Input from '../Input/Input';
+import Lists from '../Lists/Lists';
+import useManga from '../../hooks/useManga';
 import './gantt.css';
 
 function Gantt() {
+  const { lists, manga } = useManga();
+
   return (
     <div className="gantt">
+      <Chart manga={manga}></Chart>
       <Input></Input>
+      <Lists lists={lists}></Lists>
     </div>
   );
 }
 
-export default Gantt;
+export default memo(Gantt);
