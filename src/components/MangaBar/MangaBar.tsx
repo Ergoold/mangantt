8afd23a6be8d531221a1e110224396b043cf16ed
Ergoold <eran1gold@gmail.com>
2022-11-25 23:35:38 +0200

    Change cover display

--- src/components/MangaBar/MangaBar.tsx
+++ src/components/MangaBar/MangaBar.tsx
@@ -3,31 +3,54 @@ import Cover from '../Cover/Cover';
 import Range from '../../types/Range';
 import './manga-bar.css';
 
+const earliest = 2;
+const latest = 12;
+
 function MangaBar({ range }: { range: Range }) {
   const background = useMemo(
     () => range.manga[0].color ?? '#ffffff',
     [range.manga]
   );
 
-  const covers = useMemo(
-    () => range.manga.map((manga) => <Cover key={manga.id} manga={manga} />),
-    [range.manga]
+  const bounds = useMemo(
+    () => ({
+      start: range.start ?? earliest,
+      end: (range.end ?? latest - 1) + 1,
+    }),
+    [range.start, range.end]
   );
+
+  const covers = useMemo(() => {
+    const allCovers = range.manga.map((manga) => (
+      <Cover key={manga.id} manga={manga} />
+    ));
+
+    const shownCovers = bounds.end - bounds.start;
+
+    return {
+      shown: allCovers.slice(0, shownCovers),
+      hover: allCovers.slice(shownCovers),
+    };
+  }, [range.manga, bounds]);
 
   return (
     <div
       style={{
         background,
-        gridColumnStart: (range.start ?? 2) - 1,
-        gridColumnEnd: (range.end ?? 12 - 1) + 1 - 1,
+        gridColumnStart: bounds.start - earliest + 1,
+        gridColumnEnd: bounds.end - earliest + 1,
       }}
       className={`${range.start ? 'round-start' : ''} ${
         range.end ? 'round-end' : ''
       }`}
     >
       <div>
-        <div className="covers">{covers}</div>
-        <div className="hover-covers round-start round-end">{covers}</div>
+        <div className="covers">{covers.shown}</div>
+        {covers.hover.length > 0 && (
+          <div className="hover-covers round-start round-end">
+            {covers.hover}
+          </div>
+        )}
       </div>
     </div>
   );
