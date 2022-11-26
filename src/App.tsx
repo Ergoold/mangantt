a9994ca5c2b3a9f52c9aa0134ae5c67f1c6f407f
Ergoold <eran1gold@gmail.com>
2022-11-26 12:42:45 +0200

    Route

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,17 @@
 import React, { memo } from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Gantt from './components/Gantt/Gantt';
 import './app.css';
 
 function App() {
   return (
-    <div className="app">
-      <Gantt></Gantt>
-    </div>
+    <BrowserRouter>
+      <div className="app">
+        <Routes>
+          <Route path="/:username" element={<Gantt />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
