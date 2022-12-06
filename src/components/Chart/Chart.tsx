4a240b8b1a27be3f401fbf0eeb8187987eee6b4e
Ergoold <eran1gold@gmail.com>
2022-12-06 21:06:10 +0200

    Remove React import

--- src/components/Chart/Chart.tsx
+++ src/components/Chart/Chart.tsx
@@ -1,4 +1,4 @@
-import React, { memo, useMemo } from 'react';
+import { memo, useMemo } from 'react';
 import { ChartProps } from './types';
 import { MangaBar } from '..';
 import { getEarliest, getLatest, sortRanges, toRanges } from '../../utils';
--- src/components/Cover/Cover.tsx
+++ src/components/Cover/Cover.tsx
@@ -1,4 +1,4 @@
-import React, { memo } from 'react';
+import { memo } from 'react';
 import { CoverProps } from './types';
 import './cover.css';
 
