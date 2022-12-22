760530d5e5987cf8a8f77448186811a34a9e090f
Ergoold <eran1gold@gmail.com>
2022-12-22 23:40:41 +0200

    Destructure in MangaBar

--- src/components/MangaBar/MangaBar.tsx
+++ src/components/MangaBar/MangaBar.tsx
@@ -3,52 +3,52 @@ import { MangaBarProps } from './types';
 import { Cover } from '..';
 import './manga-bar.css';
 
-export const MangaBar = memo(({ range, earliest, latest }: MangaBarProps) => {
-  const bounds = useMemo(
-    () => ({
-      start: range.start ?? earliest + 1,
-      end: (range.end ?? latest - 1) + 1,
-    }),
-    [range.start, earliest, range.end, latest]
-  );
+export const MangaBar = memo(
+  ({ range: { start, end, manga }, earliest, latest }: MangaBarProps) => {
+    const bounds = useMemo(
+      () => ({
+        start: start ?? earliest + 1,
+        end: (end ?? latest - 1) + 1,
+      }),
+      [start, earliest, end, latest]
+    );
 
-  const covers = useMemo(() => {
-    const allCovers = range.manga.map((manga) => (
-      <Cover key={manga.id} manga={manga} />
-    ));
+    const { shown, hover } = useMemo(() => {
+      const allCovers = manga.map((manga) => (
+        <Cover key={manga.id} manga={manga} />
+      ));
 
-    const shownCovers = bounds.end - bounds.start;
+      const shownCovers = bounds.end - bounds.start;
 
-    return {
-      shown: allCovers.slice(0, shownCovers),
-      hover: allCovers.slice(shownCovers),
-    };
-  }, [range.manga, bounds]);
+      return {
+        shown: allCovers.slice(0, shownCovers),
+        hover: allCovers.slice(shownCovers),
+      };
+    }, [manga, bounds]);
 
-  const style = useMemo(
-    () => ({
-      background: range.manga[0].color ?? '#ffffff',
-      gridColumnStart: bounds.start - earliest,
-      gridColumnEnd: bounds.end - earliest,
-    }),
-    [range.manga, bounds, earliest]
-  );
+    const style = useMemo(
+      () => ({
+        background: manga[0].color ?? '#ffffff',
+        gridColumnStart: bounds.start - earliest,
+        gridColumnEnd: bounds.end - earliest,
+      }),
+      [manga, bounds, earliest]
+    );
 
-  const className = useMemo(
-    () => `${range.start ? 'round-start' : ''} ${range.end ? 'round-end' : ''}`,
-    [range.start, range.end]
-  );
+    const className = useMemo(
+      () => `${start ? 'round-start' : ''} ${end ? 'round-end' : ''}`,
+      [start, end]
+    );
 
-  return (
-    <div style={style} className={className}>
-      <div>
-        <div className="covers">{covers.shown}</div>
-        {covers.hover.length > 0 && (
-          <div className="hover covers round-start round-end">
-            {covers.hover}
-          </div>
-        )}
+    return (
+      <div style={style} className={className}>
+        <div>
+          <div className="covers">{shown}</div>
+          {hover.length > 0 && (
+            <div className="hover covers round-start round-end">{hover}</div>
+          )}
+        </div>
       </div>
-    </div>
-  );
-});
+    );
+  }
+);
