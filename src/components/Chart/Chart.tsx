dcdcf5cebc558da754a79d4efeaa401015b751dc
Ergoold <eran1gold@gmail.com>
2023-09-20 22:17:10 +0300

    Remove useMemo and useCallback uses

--- src/components/Chart/Chart.tsx
+++ src/components/Chart/Chart.tsx
@@ -1,23 +1,23 @@
-import { memo, useMemo } from 'react';
+import { memo } from 'react';
 import { ChartProps } from './types';
 import { MangaBar } from '..';
 import { getBounds, sortRanges, toRanges } from '../../utils';
 import './chart.css';
 
 export const Chart = memo(({ manga }: ChartProps) => {
-  const mangaBars = useMemo(() => {
-    const ranges = toRanges(manga);
-    const { earliest, latest } = getBounds(ranges);
+  const ranges = toRanges(manga);
+  const { earliest, latest } = getBounds(ranges);
 
-    return sortRanges(ranges).map((range) => (
-      <MangaBar
-        key={range.manga[0].id}
-        range={range}
-        earliest={earliest}
-        latest={latest}
-      />
-    ));
-  }, [manga]);
-
-  return <div className="chart">{mangaBars}</div>;
+  return (
+    <div className="chart">
+      {sortRanges(ranges).map((range) => (
+        <MangaBar
+          key={range.manga[0].id}
+          range={range}
+          earliest={earliest}
+          latest={latest}
+        />
+      ))}
+    </div>
+  );
 });
